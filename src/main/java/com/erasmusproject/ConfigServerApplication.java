a2331fe2353844076469520c701dc0a6bacd2919
Aleksandar Lukic <Aleksandar Lukic>
2017-02-11 16:54:31 +0100

    Updated maven groupId

--- src/main/java/com/erasmusproject/ConfigServerApplication.java
+++ src/main/java/com/erasmusproject/ConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.erasmusproj;
+package com.erasmusproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/erasmusproject/ConfigServerApplicationTests.java
+++ src/test/java/com/erasmusproject/ConfigServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.erasmusproj;
+package com.erasmusproject;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
